2e5525e31dbc57da9346b4b82ac8d97e4da99aac
ALIF-AL-RAZI <alifalrazi1@gmail.com>
2024-07-31 13:32:15 +0600

    adding more functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,185 @@
-import { useState } from 'react'
-
-import './App.css'
-
-import { domToPng } from 'modern-screenshot'
-
+import { useState, useEffect } from 'react';
+import './App.css';
+import { domToPng } from 'modern-screenshot';
 
 function App() {
-  const [headText, setHeadText] = useState("");
-  const [bodyText, setBodyText] = useState("");
-  const [image, setImage] = useState(false);
+const [headText, setHeadText] = useState("");
+const [bodyText, setBodyText] = useState("");
+const [image, setImage] = useState(false);
+const [paragraphWidth, setParagraphWidth] = useState(40);
+const [fontSize, setFontSize] = useState(20);
+const [opacity, setOpacity] = useState(0.4);
+const [bgColor, setBgColor] = useState("#fff");
+const [fontColor, setFontColor] = useState("#000");
 
-  const imageHandler = (e)=>{
-    setImage(e.target.files[0]);
+const handleBgColor = (e) => {
+setBgColor(e.target.value);
+};
+const handleFontColor = (e) => {
+  setFontColor(e.target.value);
+  };
+
+const handleWidthChange = (e) => {
+setParagraphWidth(e.target.value);
 };
 
+const handleFontSize = (e) => {
+setFontSize(e.target.value);
+};
 
-  const downloadHandler = () =>{
-  domToPng(document.querySelector('.container')).then(dataUrl => {
-    
-    const link = document.createElement('a')
-    link.download = 'screenshot.png'
-    link.href = dataUrl
-    link.click()
-  })
+const handleOpacity = (e) => {
+
+setOpacity(e.target.value);
+};
+
+const imageHandler = (e) => {
+setImage(e.target.files[0]);
+};
+
+const downloadHandler = () => {
+domToPng(document.querySelector('.container')).then(dataUrl => {
+  const link = document.createElement('a');
+  link.download = 'screenshot.png';
+  link.href = dataUrl;
+  link.click();
+});
+};
+
+useEffect(() => {
+const elmnt = document.querySelector(".paragraph");
+if (elmnt) {
+  dragElement(elmnt);
+}
+}, []);
+
+function dragElement(elmnt) {
+var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+elmnt.onmousedown = dragMouseDown;
+
+function dragMouseDown(e) {
+  e = e || window.event;
+  e.preventDefault();
+  pos3 = e.clientX;
+  pos4 = e.clientY;
+  document.onmouseup = closeDragElement;
+  document.onmousemove = elementDrag;
 }
 
+function elementDrag(e) {
+  e = e || window.event;
+  e.preventDefault();
+  pos1 = pos3 - e.clientX;
+  pos2 = pos4 - e.clientY;
+  pos3 = e.clientX;
+  pos4 = e.clientY;
 
+  var newTop = elmnt.offsetTop - pos2;
+  var newLeft = elmnt.offsetLeft - pos1;
 
+  var container = document.querySelector(".container");
+  var containerRect = container.getBoundingClientRect();
+  var elementRect = elmnt.getBoundingClientRect();
 
+  if (newTop < 0) {
+    newTop = 0;
+  } else if (newTop + elementRect.height > containerRect.height) {
+    newTop = containerRect.height - elementRect.height;
+  }
 
+  if (newLeft < 0) {
+    newLeft = 0;
+  } else if (newLeft + elementRect.width > containerRect.width) {
+    newLeft = containerRect.width - elementRect.width;
+  }
 
-  // const downloadHandler = () =>{
-  //       html2canvas(document.querySelector(".container"), 
-  //       {allowTaint: true, useCors: true}).then(canvas => {
-  //     document.querySelector('.preview-container').appendChild(canvas)
+  elmnt.style.top = newTop + "px";
+  elmnt.style.left = newLeft + "px";
+}
+
+function closeDragElement() {
+  document.onmouseup = null;
+  document.onmousemove = null;
+}
+}
+
+return (
+<div className="page">
+  <div className="input">
+    <input 
+      className="header-input" 
+      type="text" 
+      onChange={(e) => setHeadText(e.target.value)} 
+      placeholder='Writer Header...'
+    />
+    <label htmlFor='file-input'>
+      <img src={image ? URL.createObjectURL(image) : "./upload_area.svg"} className='addproduct-thumnail-img' alt=''/>
+      <input onChange={imageHandler} type='file' name='image' id='file-input' hidden/>
+    </label>
+    <textarea name="" className="body-input" 
+      type="text" 
+      onChange={(e) => setBodyText(e.target.value)}
+      placeholder='Writer Content...'>
+    </textarea>
+    <label htmlFor="width-slider">Adjust Paragraph Width:</label>
+      <input 
+        id="width-slider" 
+        type="range" 
+        min="40" 
+        max="100" 
+        value={paragraphWidth} 
+        onChange={handleWidthChange} 
+      />
+
+<label htmlFor="fontSize-slider">Adjust Font Size:</label>
+      <input 
+        id="fontSize-slider" 
+        type="range" 
+        min="20" 
+        max="70" 
+        value={fontSize} 
+        onChange={handleFontSize} 
+      />
+      <label htmlFor="opacity-slider">Adjust Image Opacity:</label>
+      <input 
+        id="opacity-slider" 
+        type="range" 
+        min="0" 
+        max="1" 
+        step="0.1"
+        value={opacity} 
+        onChange={handleOpacity} 
+      />
+
+<label htmlFor="bgColor">Select Background color:</label>
+<input type="color" id="bgColor" value={bgColor} onChange={handleBgColor}/>
+
+<label htmlFor="fontcolor">Select Font color:</label>
+<input type="color" id="fontcolor" value={fontColor} onChange={handleFontColor}/>
+
       
-  //   });
-  //     }
+  </div>   
+  
 
-
-      // const download = document.querySelector(".capture-button");
-      // download.addEventListener("click", downloadHandler());
-
-  return (
-    <>
-
-    <div className="page">
-      <div className="input">
-        <input 
-          className="header-input" 
-          type="text" 
-          onChange={(e)=>setHeadText(e.target.value)} 
-          placeholder='Writer Header...'
-        />
-        <label htmlFor='file-input'>
-                <img src={image?URL.createObjectURL(image):"./upload_area.svg"} className='addproduct-thumnail-img' alt=''/>
-                <input onChange={imageHandler} type='file' name='image' id='file-input' hidden/>
-        </label>
-        <textarea name="" className="body-input" 
-          type="text" 
-          onChange={(e)=>setBodyText(e.target.value)}
-          placeholder='Writer Content...'>
-        </textarea>
-        
-      </div>   
-
-      <div className="container">
-          <h1>{headText}</h1>
-          
-          <img src={image?URL.createObjectURL(image):"./HEROSCREEN-WALLPAPER-4K-TIGER.jpg"} alt=""/>
-          <p>
-            {bodyText}
-          </p>
-      </div>
-        
-      
-      <div className="buttons">
-        <button className="capture-button" onClick={downloadHandler}>Capture Screenshot</button>
-      </div>
+  <div className="container">
+    <h1>{headText}</h1>
+    <p className="paragraph" style={{position: 'absolute', width: `${paragraphWidth}%`, fontSize: `${fontSize}px`, color:`${fontColor}`}}>
+      {bodyText}
+    </p>
+    <div className="img" style={{backgroundColor: `${bgColor}`}}>
+      <img 
+      src={image ? URL.createObjectURL(image) : "./plain-white-background-xdwiksazlsuzkuhi.jpg"} 
+      alt=""
+      style={{opacity:`${opacity}`}}
+      />
     </div>
 
-    
-    </>
-  )
+  </div>
+
+  <div className="buttons">
+    <button className="capture-button" onClick={downloadHandler}>Capture Screenshot</button>
+  </div>
+</div>
+);
 }
 
-export default App
+export default App;
